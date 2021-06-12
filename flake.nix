eef518d3c8f9574ed2d4b0ed359e0e59cb63db21
zseri <zseri.devel@ytrizja.de>
2021-06-13 01:27:01 +0200

    put `lib` attribute into separate file

--- flake.nix
+++ flake.nix
@@ -1,34 +1,7 @@
 {
   description = "yet another Nix flake-utils library";
   outputs = { self }: {
-    lib = rec {
-      # inlined from github:numtide/flake-utils
-      eachSystem = systems: f:
-        let
-          op = attrs: system:
-            let
-              ret = f system;
-              op = attrs: key:
-                attrs //
-                {
-                  ${key} = (attrs.${key} or { }) // { ${system} = ret.${key}; };
-                }
-              ;
-            in
-            builtins.foldl' op attrs (builtins.attrNames ret);
-        in
-        builtins.foldl' op { } systems;
-
-      mkFlake = import ./mkFlake.nix { inherit eachSystem; };
-      mkFlakeFromProg =
-        { prevpkgs, progname, drvBuilder
-        , systems ? prevpkgs.lib.platforms.all
-        }: mkFlake rec {
-          inherit prevpkgs systems;
-          overlay = final: prev: { ${progname} = drvBuilder final prev; };
-          defaultProgName = progname;
-        };
-    };
+    lib = import lib.nix;
 
     templates = {
       rust = {
